3831134af97dc05273643ef4cadcc446154ece7e
David James Carter <fresco.raja@gmail.com>
2016-04-29 03:03:39 -0700

    added firefox moz-opaque attr to canvas for optimization

--- app/assets/javascripts/sounds.js
+++ app/assets/javascripts/sounds.js
@@ -4,11 +4,11 @@
   }
 
   var Sounds = CrappyBird.Sounds = function () {
-    this.fly = new Audio("assets/sfx_wing.mov");
-    this.point = new Audio("assets/sfx_point.mov");
-    this.hit = new Audio("assets/sfx_hit.mov");
-    this.swooshing = new Audio("assets/sfx_swooshing.mov");
-    this.die = new Audio("assets/sfx_die.mov");
-    this.poo = new Audio("assets/fart.mov");
+    this.fly = new Audio("assets/sfx_wing.mp3");
+    this.point = new Audio("assets/sfx_point.mp3");
+    this.hit = new Audio("assets/sfx_hit.mp3");
+    this.swooshing = new Audio("assets/sfx_swooshing.mp3");
+    this.die = new Audio("assets/sfx_die.mp3");
+    this.poo = new Audio("assets/fart.mp3");
   };
 })();
